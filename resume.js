113274b0f8f04e35a3dab35b5430512f5240237c
PoojithaPooja20 <118331512+PoojithaPooja20@users.noreply.github.com>
2022-12-15 10:12:20 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,10 @@
+ functionresume(){
+    let firstname;
+    firstname=document.getElementById("firstname").value;
+    console.log(firstname);
+    let lastname;
+    lastname=document.getElementById("lastname").value;
+    console.log(lastname);
+    document.getElementById("name").innerHTML=firstname+lastname;
+}
+
